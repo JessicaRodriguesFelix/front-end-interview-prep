5500eb82d503b365d08bd3ea09e0cd4314e64810
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-12-07 13:44:05 -0500

    add examples of higher-order functions and function invocation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 function testCode() {
+
+  let y = 100
   
   function addOne(x) {
     return x + 1
   }
-  let whatAmI = addOne
-  addOne = (x) => x + 2
 
+  let whatAmI = addOne
+  console.log(addOne(1))
+
+  addOne = (x, y) => x + y
+
+  console.log(addOne(10,20))
+  console.log(addOne(100));
 
   console.log(whatAmI(10));
 
 }
-  testCode(); // console.log 11 - it calls the closest addOne function
+
+function addFive(x) {
+    return x + 5;
+}
+
+function multiplyByThree(n) {
+  return n * 10;
+}
+
+function multiplyByTen(n) {
+  return n * 3;
+}
+
+function compose(applyFirst, applySecond) {
+  let myInternalFunc = number => {
+    let x = 10
+    return applySecond(applyFirst(number))
+  }
+  applyFirst = multiplyByTen
+  applySecond = multiplyByTen
+  return myInternalFunc
+}
+/* uncommet line below for testing purposes */
+// testCode(); // console.log 11 - it calls the closest addOne function
+
+  // compose() = myInternalFunction
+  // console.log(compose()(8))
+  // console.log(compose(addFive, multiplyByThree)(8))
+  
+function tricky(x) {
+  console.log(x)
+  console.log("tricky")
+}
+
+function extraTricky(){
+  return tricky
+}
+
+extraTricky()(console.log('hello from line 58'))
